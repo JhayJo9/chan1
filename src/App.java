74897a6eb7773531d483ca03018c0ddd28f1a85c
JhayJo9 <penalozajhay8@gmail.com>
2023-09-20 04:42:38 -0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.text.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+       Scanner scan = new Scanner(System.in);
+       int monthlyOfYear = 12;
+       int principal;
+       double r_interestRate;
+       int yrs;
+    
+       System.out.print("Enter Principal : ");
+       principal = scan.nextInt();
+
+       System.out.print("Annual interest rate : ");
+       r_interestRate = scan.nextDouble();
+       double monthlyInterest = r_interestRate / 100 / monthlyOfYear;
+       System.out.print("Period (years) : ");
+       yrs = scan.nextInt();
+       int NumsOfPayments = yrs * monthlyOfYear;
+       double Mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, NumsOfPayments) )
+       /( Math.pow(1 +monthlyInterest , NumsOfPayments)- 1);
+       System.out.println("Mortgage : " + Mortgage);
+
+    }
+}
