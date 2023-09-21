834673caa1a481422c5c0eca3fc0a42ddf5ee306
JhayJo9 <penalozajhay8@gmail.com>
2023-09-20 19:29:56 -0700

    mortgage with control flow

--- src/App.java
+++ src/App.java
@@ -3,22 +3,52 @@ import java.text.*;
 public class App {
     public static void main(String[] args) throws Exception {
        Scanner scan = new Scanner(System.in);
-       int monthlyOfYear = 12;
+       final int monthlyOfYear = 12;
        int principal;
        double r_interestRate;
        int yrs;
-    
-       System.out.print("Enter Principal : ");
-       principal = scan.nextInt();
 
+       while(true){
+       System.out.print("Enter Principal  ( 1k to 1m): ");
+       principal = scan.nextInt();
+       if(principal < 1000 || principal > 1000000){
+         System.out.println("Please enter greater than 1k or less than 1m ");
+          continue;
+       }
+       else{
+         break;
+       }
+
+       }
+       while(true){
        System.out.print("Annual interest rate : ");
        r_interestRate = scan.nextDouble();
-       double monthlyInterest = r_interestRate / 100 / monthlyOfYear;
+      
+       if(r_interestRate <= 0){
+         System.out.println("Please enter greater than > 0 or less than < 30 ");
+         continue;
+       }
+       else {
+       break;
+       }
+       }
+       while(true){
        System.out.print("Period (years) : ");
        yrs = scan.nextInt();
+       if(yrs <= 0){
+        System.out.println("Please enter greater > 0 or less than < 30");
+        continue;
+       }
+       else{
+        break;
+       }
+       }
+
+       double monthlyInterest = r_interestRate / 100 / monthlyOfYear;
        int NumsOfPayments = yrs * monthlyOfYear;
        double Mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, NumsOfPayments) )
        /( Math.pow(1 +monthlyInterest , NumsOfPayments)- 1);
-       System.out.println("Mortgage : " + Mortgage);
+       String MortgageFormatted = NumberFormat.getCurrencyInstance().format(Mortgage);
+       System.out.println("Mortgage : " + MortgageFormatted);
     }
 }
